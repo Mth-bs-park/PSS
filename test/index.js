1b352612864fa2723f23cbd8e826ac97f2113ae8
Methew <bbeomssoo@gmail.com>
2017-09-14 13:06:16 +0900

    init

--- test/index.js
+++ test/index.js
@@ -1,39 +1,32 @@
 const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
-const config = require('./config/mail.json');
+const config = require('./config/user.json');
 
-const stmpRunner = {
+const smtp = config.SMTP;
 
-    run: function() {
+// const {user, password: pass} = stmp.auth;
+const user = smtp.auth.user;
+const pass = smtp.auth.password;
 
-        const smtp = config.SMTP;
-
-        const user = smtp.auth.user;
-        const pass = smtp.auth.password;
-        // const {user, password: pass} = stmp.auth;
-
-        const transport = nodemailer.createTransport(smtpTransport({
-            service: smtp.service,
-            auth: {
-                user: `${user}@gmail.com`,
-                pass: pass
-            }
-        }));
-
-        const mailOptions = {
-            from: `BOT <${user}@gmail.com>`,
-            to: `${config.mail.from.join(',')}`,
-            subject: 'Nodemailer 테스트',
-            text: '평문 보내기 테스트 '
-        };
-
-        transport.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error)
-            }
-            console.log('Message %s sent: %s', info.messageId, info.response)
-        });
+const transport = nodemailer.createTransport(smtpTransport({
+    service: smtp.service,
+    auth: {
+        user: `${user}@gmail.com`,
+        pass: pass
     }
+}));
+
+const mailOptions = {
+    from: `BOT <${user}@gmail.com>`,
+    to: `${config.mail.from.join(',')}`,
+    subject: 'Nodemailer 테스트입니다.',
+    html: '<h1>test</h1><div>test 입니다.</div>'
 };
 
-module.exports = stmpRunner;
+transport.sendMail(mailOptions, (error, info) => {
+    
+    if (error) {
+        return console.log(error)
+    }
+    console.log('Message %s sent: %s', info.accepted, info.response)
+});
