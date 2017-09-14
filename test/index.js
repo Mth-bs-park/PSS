4830e2fefaa39b6c1b82f0d90b16f9b24fdc5b55
Methew <bbeomssoo@gmail.com>
2017-09-14 11:29:04 +0900

    init

---
+++ test/index.js
@@ -0,0 +1,39 @@
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
+const config = require('./config/mail.json');
+
+const stmpRunner = {
+
+    run: function() {
+
+        const smtp = config.SMTP;
+
+        const user = smtp.auth.user;
+        const pass = smtp.auth.password;
+        // const {user, password: pass} = stmp.auth;
+
+        const transport = nodemailer.createTransport(smtpTransport({
+            service: smtp.service,
+            auth: {
+                user: `${user}@gmail.com`,
+                pass: pass
+            }
+        }));
+
+        const mailOptions = {
+            from: `BXUIP BOT <${user}@gmail.com>`,
+            to: `${config.mail.from.join(',')}`,
+            subject: 'Nodemailer 테스트',
+            text: '평문 보내기 테스트 '
+        };
+
+        transport.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response)
+    });
+    }
+};
+
+module.exports = stmpRunner;
